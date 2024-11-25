e4a68151f23806102f263bda118b60372e9fbc89
joelliusczar <2503795+joelliusczar@users.noreply.github.com>
2024-11-24 21:06:38 -0500

    Fixed an issue with canary

--- lib/canary/canary.rb
+++ lib/canary/canary.rb
@@ -2,7 +2,7 @@ module SaladPrep
 	module Canary
 
 		def self.version
-			gemspec = File.absolute_path('../salad_prep.gemspec')
+			gemspec = File.expand_path("../../../salad_prep.gemspec",__FILE__)
 			Gem::Specification.load(gemspec).version
 		end
 
