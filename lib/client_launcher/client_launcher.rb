8f10bfd2084a3b0b27712fae189a18a4bd58c568
joelliusczar <2503795+joelliusczar@users.noreply.github.com>
2024-11-30 22:43:51 -0500

    Client setup wasn't calling the script

--- lib/client_launcher/client_launcher.rb
+++ lib/client_launcher/client_launcher.rb
@@ -1,5 +1,6 @@
 require "fileutils"
 require_relative "../file_herder/file_herder"
+require_relative "../arg_checker/arg_checker"
 
 module SaladPrep
 	class ClientLauncher
@@ -8,6 +9,8 @@ module SaladPrep
 		end
 
 		def setup_client()
+			ArgChecker.path(@egg.client_dest)
+			ArgChecker.path(@egg.full_url)
 			FileHerder::empty_dir(@egg.client_dest)
 			script = <<~CALL
 					if [ -z "$NVM_DIR" ]; then
@@ -18,10 +21,11 @@ module SaladPrep
 					export VITE_BASE_ADDRESS="#{@egg.full_url}" &&
 					#set up react then copy
 					#install packages
-					npm --prefix #{@egg.client_src} i &&
+					npm --prefix '#{@egg.client_src}' i &&
 					#build code (transpile it)
 					npm run --prefix '#{@egg.client_src}' build &&
 			CALL
+			system(script, exception: true)
 			FileUtils.cp_r(
 				File.join(@egg.client_src, "build/"),
 				@egg.client_dest
