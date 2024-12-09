3563b564b55924382119269d3845ebb804611acd
joelliusczar <2503795+joelliusczar@users.noreply.github.com>
2024-12-08 21:28:39 -0500

    Forgot to include execution permissions

--- lib/installion/binstallion.rb
+++ lib/installion/binstallion.rb
@@ -1,3 +1,4 @@
+require "fileutils"
 require_relative "../box_box/box_box"
 require_relative "../file_herder/file_herder"
 require_relative "../resorcerer/resorcerer"
@@ -14,6 +15,17 @@ module SaladPrep
 			BoxBox.path_append(@egg.dev_ops_bin)
 		end
 
+		def install_script(name, action_body)
+			file_path = File.join(@egg.dev_ops_bin, name)
+			File.open(file_path, "w").write(
+				Resorcerer.bin_wrapper_template_compile(
+					File.open(@template_context_path).read,
+					action_body
+				)
+			)
+			FileUtils.chmod("a+x", file_path)
+		end
+
 		def install_py_env_if_needed
 			action_body = <<~CODE
 				if args_hash.include?("testing")
@@ -24,15 +36,7 @@ module SaladPrep
 					Provincial.monty.install_py_env_if_needed
 				end
 			CODE
-			File.open(
-				File.join(@egg.dev_ops_bin, "#{@egg.env_prefix}_bootstrap"),
-				"w"
-			).write(
-				Resorcerer.bin_wrapper_template_compile(
-					File.open(@template_context_path).read,
-					action_body
-				)
-			)
+			install_script("#{@egg.env_prefix}_bootstrap", action_body)
 		end
 
 	end
