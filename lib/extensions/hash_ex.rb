bd16de515e7318f199abb059663a7185ecb35d0f
joelliusczar <2503795+joelliusczar@users.noreply.github.com>
2025-01-03 12:38:17 -0500

    Had no previously passed Hash to refine

--- lib/extensions/hash_ex.rb
+++ lib/extensions/hash_ex.rb
@@ -1,21 +1,24 @@
 module SaladPrep
 	module HashEx
-
-		alias_method :__access__, :[]
-
-		def coalesce(*keys)
-			keys.each do |key|
-				value = __access__(key)
-				if ! value.nil?
-					return value
+		refine Hash do
+		
+			alias_method :__access__, :[]
+	
+			def coalesce(*keys)
+				keys.each do |key|
+					value = __access__(key)
+					if ! value.nil?
+						return value
+					end
 				end
+				nil
 			end
-			nil
-		end
-
-		def include?(*keys)
-			keys.each { |key| return true if has_key(key) }
-			false
+	
+			def include?(*keys)
+				keys.each { |key| return true if has_key(key) }
+				false
+			end
+			
 		end
 
 	end
